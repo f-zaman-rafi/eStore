89fed3d5174da4533742815188cccc0134301b96
RAFI <gollarafi@gmail.com>
2024-07-19 03:51:08 +0800

    added other sign in method and logout method with firebase

--- src/Providers/AuthProviders.jsx
+++ src/Providers/AuthProviders.jsx
@@ -1,25 +1,61 @@
 /* eslint-disable react/prop-types */
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { createUserWithEmailAndPassword, GithubAuthProvider, GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
 import { createContext, useState } from "react";
 import auth from "../Firebase/firebase.config";
 
 export const AuthContext = createContext(null);
 
 const googleProvider = new GoogleAuthProvider();
+const gitHubProvider = new GithubAuthProvider();
 
 const AuthProvider = ({ children }) => {
-
+    const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(null);
 
+    // create user with email
+
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    // sign in with email
+
+    const signIn = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    // sign in with google
 
     const signInWithGoogle = () => {
         setLoading(true);
         return signInWithPopup(auth, googleProvider);
     }
 
+    // sign in with github
+
+    const signInWithGitHub = () => {
+        setLoading(true);
+        return signInWithPopup(auth, gitHubProvider)
+    }
+
+    // logout
+
+    const logout = () => {
+        setLoading(true);
+        return signOut(auth);
+    }
+
     const authInfo = {
         loading,
-        signInWithGoogle
+        user,
+        createUser,
+        signIn,
+        signInWithGoogle,
+        signInWithGitHub,
+        logout
+
     };
     return (
         <AuthContext.Provider value={authInfo}>
