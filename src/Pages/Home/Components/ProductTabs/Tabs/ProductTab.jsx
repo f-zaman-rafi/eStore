0331d157c48e6244e969760636794c30fd2e1428
RAFI <gollarafi@gmail.com>
2024-08-01 21:34:10 +0800

    added products tabs

---
+++ src/Pages/Home/Components/ProductTabs/Tabs/ProductTab.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+const ProductTab = () => {
+
+    const [activeTab, setActiveTab] = useState('new-arrival');
+
+    const handleTabs = (tab) => {
+        setActiveTab(tab);
+    }
+    console.log(activeTab)
+
+
+    return (
+        <div className="lg:mx-40 lg:mt-14">
+            <div className="text-sm font-medium text-center text-gray-500 ">
+                <ul className="flex gap-8">
+                    <li className="me-2">
+                        <a onClick={() => handleTabs('new-arrival')} className={`inline-block text-lg font-semibold ${activeTab === 'new-arrival' ? 'border-black border-b-[3px] text-black font-bold ' : 'border-transparent text-gray-500'}`} >New Arrival</a>
+                    </li>
+                    <li className="me-2">
+                        <a onClick={() => handleTabs('best-seller')} className={`inline-block text-lg font-semibold ${activeTab === 'best-seller' ? 'border-black border-b-[3px] text-black font-bold ' : 'border-transparent text-gray-500'}`}>Best Seller</a>
+                    </li>
+                    <li className="me-2">
+                        <a onClick={() => handleTabs('featured-products')} className={`inline-block text-lg font-semibold ${activeTab === 'featured-products' ? 'border-black border-b-[3px] text-black font-bold ' : 'border-transparent text-gray-500'}`} >Featured Products</a>
+                    </li>
+
+                </ul>
+            </div>
+
+        </div>
+    );
+};
+
+export default ProductTab;
--- src/Pages/Home/Homepage/Home.jsx
+++ src/Pages/Home/Homepage/Home.jsx
@@ -1,6 +1,7 @@
 import Banner from "../Components/Banner/Banner";
 import BrowseByCatagory from "../Components/BrowseByCatagory/BrowseByCatagory";
 import Collage from "../Components/ProductCollage/Collage";
+import ProductTab from "../Components/ProductTabs/Tabs/ProductTab";
 
 const Home = () => {
     return (
@@ -8,6 +9,7 @@ const Home = () => {
             <Banner />
             <Collage />
             <BrowseByCatagory />
+            <ProductTab />
         </div>
     );
 };
