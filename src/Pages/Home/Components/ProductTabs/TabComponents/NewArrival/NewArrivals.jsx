185dfd45ae0cb93dea56c2d64f83cda9dc12da80
RAFI <gollarafi@gmail.com>
2024-10-23 03:41:50 +0800

    getting api of all sorted products from backend

--- src/Pages/Home/Components/ProductTabs/TabComponents/NewArrival/NewArrivals.jsx
+++ src/Pages/Home/Components/ProductTabs/TabComponents/NewArrival/NewArrivals.jsx
@@ -2,37 +2,47 @@ import useAxiosCommon from "../../../../../../Hooks/useAxiosCommon";
 import { useQuery } from "@tanstack/react-query";
 import LoadingComponent from "../../../../../../SharedComponents/Loading/LoadingComponent";
 
-
-
 const NewArrivals = () => {
+
     const axiosCommon = useAxiosCommon();
 
     const { data: productData = [], isLoading } = useQuery({
-        queryKey: ['productData'],
+        queryKey: ['allProducts'],
         queryFn: async () => {
-            const { data } = await axiosCommon.get(`/products`);
-            return data;
+            const [phones, smartWatches, cameras, headphones, computers, consoles] = await Promise.all([
+                axiosCommon.get(`/phones?status=new_arrival`),
+                axiosCommon.get(`/smartWatches?status=new_arrival`),
+                axiosCommon.get(`/cameras?status=new_arrival`),
+                axiosCommon.get(`/headphones?status=new_arrival`),
+                axiosCommon.get(`/computers?status=new_arrival`),
+                axiosCommon.get(`/consoles?status=new_arrival`),
+            ]);
+
+            // combine all data into a single array with spread operator
+            return [
+                ...phones.data,
+                ...smartWatches.data,
+                ...cameras.data,
+                ...headphones.data,
+                ...computers.data,
+                ...consoles.data
+            ];
         }
     });
 
     if (isLoading) return <LoadingComponent />;
     console.log(productData);
 
-
-
-
     return (
         <div>
-            <p>total products:  {productData.length}</p>
+            <p>total products: {productData.length}</p>
+            {productData.map(product => (
+                <div key={product._id} className="border-2 p-4 m-4">
+                    <img className="h-40 w-40" src={product.image} />
+                    <h3>{product.Title}</h3>
+                </div>
+            ))
 
-            {
-                productData.map(product => (
-                    <div key={product._id}>
-                        <img className="h-40 w-40" src={product.image} alt="" />
-                        <p>{product.title}</p>
-                        <p>{product.ram}</p>
-                    </div>
-                ))
             }
         </div>
     );
