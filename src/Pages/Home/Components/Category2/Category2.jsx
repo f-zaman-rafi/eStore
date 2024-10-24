58da93cda6f22b93fb546fe9b6a0407ef8f64a12
RAFI <gollarafi@gmail.com>
2024-10-24 22:36:14 +0800

    category 2 design done

--- src/Pages/Home/Components/Category2/Category2.jsx
+++ src/Pages/Home/Components/Category2/Category2.jsx
@@ -8,46 +8,46 @@ const Category2 = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mt-14">
             <div className=''>
                 <div className='flex justify-center h-72'>
-                    <img className='w-72 mr-[-200px]' src={airpod} alt="" />
+                    <img className='w-72 h-full mr-[-200px]' src={airpod} alt="" />
                     <img className='w-[164px] h-[168px] mt-20' src={watch} alt="" />
                 </div>
-                <div className='space-y-4 mx-8 px-4 text-left border-2'>
+                <div className='space-y-4 px-8 text-left h-auto w-full pb-14'>
                     <p className='text-2xl my-4'>Popular Products</p>
-                    <p className='font-medium my-auto text-sm'>iPad combines a magnificent 10.2-inch Retina display, incredible performance, multitasking and ease of use.</p>
-                    <p className='btn btn-outline border-gray-100 px-14'>Shop Now</p>
+                    <p className='font-medium pt-4 pb-3 text-sm'>iPad combines a magnificent 10.2-inch Retina display, incredible performance, multitasking and ease of use.</p>
+                    <p className='btn btn-outline border-gray-100 px-14 py-4'>Shop Now</p>
                 </div>
             </div>
 
-            <div>
+            <div className='bg-[#F9F9F9]'>
                 <div className='h-72'>
-                    <img className='w-72' src={ipadPro} alt="" />
+                    <img className='w-72 h-full' src={ipadPro} alt="" />
                 </div>
-                <div className='space-y-4 mx-8 px-4 text-left border-2'>
+                <div className='space-y-4 px-8 text-left h-auto w-full pb-14'>
                     <p className='text-2xl my-4'>Ipad Pro</p>
-                    <p className='font-medium my-auto text-sm'> iPad combines a magnificent 10.2-inch Retina display, incredible performance, multitasking and ease of use.</p>
-                    <p className='btn btn-outline border-gray-100 px-14'>Shop Now</p>
+                    <p className='font-medium pt-4 pb-3 text-sm'> iPad combines a magnificent 10.2-inch Retina display, incredible performance, multitasking and ease of use.</p>
+                    <p className='btn btn-outline border-gray-100 px-14 py-4'>Shop Now</p>
                 </div>
             </div>
 
-            <div>
+            <div className='bg-[#EAEAEA]'>
                 <div className='h-72'>
-                    <img className='w-72' src={galaxy} alt="" />
+                    <img className='w-72 h-full' src={galaxy} alt="" />
                 </div>
-                <div className='space-y-4 mx-8 px-4 text-left border-2'>
-                    <p className='text-2xl my-4'>Ipad Pro</p>
-                    <p className='font-medium my-auto text-sm'> iPad combines a magnificent 10.2-inch Retina display, incredible performance, multitasking and ease of use.</p>
-                    <p className='btn btn-outline border-gray-100 px-14'>Shop Now</p>
+                <div className='space-y-4 px-8 text-left h-auto w-full pb-14'>
+                    <p className='text-2xl my-4'>Samsung Galaxy</p>
+                    <p className='font-medium pt-4 pb-3 text-sm'> iPad combines a magnificent 10.2-inch Retina display, incredible performance, multitasking and ease of use.</p>
+                    <p className='btn btn-outline border-gray-100 px-14 py-4'>Shop Now</p>
                 </div>
             </div>
 
-            <div>
+            <div className='bg-[#2C2C2C]'>
                 <div className='h-72'>
-                    <img className='w-72' src={macbook} alt="" />
+                    <img className='w-72 h-full' src={macbook} alt="" />
                 </div>
-                <div className='space-y-4 mx-8 px-4 text-left border-2'>
-                    <p className='text-2xl my-4'>Ipad Pro</p>
-                    <p className='font-medium my-auto text-sm'> iPad combines a magnificent 10.2-inch Retina display, incredible performance, multitasking and ease of use.</p>
-                    <p className='btn btn-outline border-gray-100 px-14'>Shop Now</p>
+                <div className='space-y-4 px-8 text-left h-auto w-full pb-14'>
+                    <p className='text-2xl my-4 text-white'>Macbook Pro</p>
+                    <p className='font-medium pt-4 pb-3 text-sm text-[#919191]'> iPad combines a magnificent 10.2-inch Retina display, incredible performance, multitasking and ease of use.</p>
+                    <p className='btn btn-outline border-gray-100 px-14 py-4 text-white'>Shop Now</p>
                 </div>
             </div>
 
